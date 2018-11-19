c706f73d746f538f080e6487643eb45ecc33bf5d
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2018-11-19 18:00:17 +0200

    Convert test expressions to snake_case
    3.1.alpha.Xrayez.634aa4f

--- tests/scripts/test_expression_builder.gd
+++ tests/scripts/test_expression_builder.gd
@@ -5,16 +5,16 @@ func _init():
 
 	var expression = String()
 
-	expression = "select(0, 1, scaleY(scaleX(gradientBasis(2, 2), 5), 5), 0.15, 0)"
+	expression = "select(0, 1, scale_y(scale_x(gradient_basis(2, 2), 5), 5), 0.15, 0)"
 	n.function = n.evaluate(expression)
 
 	n.get_color_2d(10.0, 20.0)
 
-	expression = "select(0, 1, scaleY(scaleX(translate(gradientBasis(2, 2), simplexBasis(3)), 5), 5), valueBasis(2, 8), 0)"
+	expression = "select(0, 1, scale_y(scale_x(translate(gradient_basis(2, 2), simplex_basis(3)), 5), 5), value_basis(2, 8), 0)"
 	n.function = n.evaluate(expression)
 	n.get_color_3d(10.0, 20.0, 30.0)
 
-	expression = "select(0, 1, sin(cos(scale(gradientBasis(select(0, 1, translateY(translateX(radial, -0.885), -0.5), 0.4, 0.0), 10), 10))), 0.841, 0)"
+	expression = "select(0, 1, sin(cos(scale(gradient_basis(select(0, 1, translate_y(translate_x(radial, -0.885), -0.5), 0.4, 0.0), 10), 10))), 0.841, 0)"
 	n.function = n.evaluate(expression)
 	n.get_color_6d(15.0, 17.8, 46.35, 7.1, 65.3, 47.2)
 
